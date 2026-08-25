06201fa476d316959571aae0b3a35b15b0a3ff40
vidhan_verma <vidhanverma2311@gmail.com>
2026-08-25 18:43:54 +0530

    small fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,4 @@ import "dotenv/config";
 import app from "./app";
 const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.listen(PORT, () => {});
